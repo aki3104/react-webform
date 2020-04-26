b5ec588d22e32d1c5503ccf137b81583fa37e320
Satoshi <aki319809@gmail.com>
2020-04-26 11:28:46 +0900

    add:material-ui

--- src/index.js
+++ src/index.js
@@ -15,3 +15,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//リロードせずに開発
+if (module.hot){
+  module.hot.accept()
+}
